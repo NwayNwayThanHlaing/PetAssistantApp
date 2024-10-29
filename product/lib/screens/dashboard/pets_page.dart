67997f0c32deee0e0b4a367829dcec941217979e
Nway Nway Than Hlaing <wlis130@live.rhul.ac.uk>
2024-10-29 20:13:51 +0000

    added dashboard screens layout

---
+++ product/lib/screens/dashboard/pets_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PetsPage extends StatelessWidget {
+  const PetsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Pets Page',
+      ),
+    );
+  }
+}
---
+++ product/lib/screens/dashboard/vet_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VetPage extends StatelessWidget {
+  const VetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Vet Page',
+      ),
+    );
+  }
+}
